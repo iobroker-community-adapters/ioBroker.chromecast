62101474033e3c4b73f29023c7fb25de8871ccba
Angel Nunez Mencias <ANUNEZ@de.ibm.com>
2016-01-03 15:34:43 +0100

    Support multiple devices

--- widgets/chromecast/js/chromecast.js
+++ widgets/chromecast/js/chromecast.js
@@ -37,10 +37,6 @@ $.extend(true, systemDictionary, {
 
 //Helpers
 function getValidDevId(devId){
-    if (vis.objects && !(devId in vis.objects)){
-        console.log("Error: "+devId+" object not found");
-        return undefined;
-    }
     
     var devIdParts = devId.split(".");
     if (devIdParts.length < 3) {
@@ -53,7 +49,50 @@ function getValidDevId(devId){
         return undefined;
     }
     
-    return devIdParts[0]+'.'+devIdParts[1]+'.'+devIdParts[2];
+    var normDevice =  devIdParts[0]+'.'+devIdParts[1]+'.'+devIdParts[2];
+    
+    if (vis.states[normDevice+".status.connected.val"] === undefined){
+        console.log("Error: "+normDevice+".status.connected not found");
+        return undefined;
+    }
+    
+    return normDevice;
+    
+}
+
+function registerForDeviceUpdates($widget, ioBrokerState){
+    //Check if state exits
+    if (vis.states[ioBrokerState+'.val'] === undefined) {
+        console.log("ERROR: "+ioBrokerState+" not available.")
+        setWidget(undefined);
+    } else{
+        //register for iobroker state updates
+        vis.states.bind(ioBrokerState + '.val', function (e, newVal, oldVal) {
+            console.log("Updated "+ioBrokerState+": "+newVal);
+            setWidget(newVal);
+        });
+        setWidget(vis.states[ioBrokerState + '.val']);
+
+        //register for widget state updates
+        $widget.change(function (evt) {
+            console.log("Sending to "+ioBrokerState+": "+ getWidgetVal());
+            vis.setValue(ioBrokerState, getWidgetVal());
+        });
+    }
+    
+    function setWidget(newVal){
+        if ($widget.attr('type') == 'checkbox')
+            $widget.prop('checked', newVal);
+        else
+            $widget.val(newVal);
+    }
+    
+    function getWidgetVal(){
+        if ($widget.attr('type') == 'checkbox')
+            return $widget.prop('checked');
+        else
+            return $widget.val();
+    }
 }
 
 // this code can be placed directly in chromecast.html
@@ -75,50 +114,83 @@ vis.binds.chromecast = {
                     vis.binds.chromecast.ChromecastDevice.init(widgetID, view, data, style);
                 }, 100);
             }
-            
-            var validDevId = getValidDevId(data.oid_dev_1);
-            var name = (validDevId)?validDevId.split(".")[2]:"Not set";
-            
-            $div.append('<center><h1>' + name + '</h1></center>');
-            
-            if (validDevId){
-                console.log("creating "+validDevId);
-                //connected - create
-                $div.append('<input class="clientConnected" type="checkbox"/>Connected</br>');
-                var $connected = $div.find('input.clientConnected');
-                //connected - register for iobroker state updates
-                var ioBrokerState = validDevId+".status.connected";
-                vis.states.bind(ioBrokerState + '.val', function (e, newVal, oldVal) {
-                    console.log("Updated "+ioBrokerState);
-                    $connected.prop('checked', newVal);
-                });
-                $connected.prop('checked', vis.states[ioBrokerState + '.val']);
+
+            $div.append('<select class="chromecastDeviceSelector"/>');
+            var $select = $div.find('select.chromecastDeviceSelector');
+            for (var devIndex=1; devIndex <= data.nDevs; devIndex++){
+                var validDevId = undefined;
+                try {
+                    validDevId = getValidDevId(data["oid_dev_"+devIndex]);                
+                } catch (e){};
                 
-                $connected.change(function (evt) {
-                    console.log("connected: "+$(this).prop('checked'));
-                    vis.setValue(ioBrokerState, $(this).prop('checked'));
-                });
+                if (validDevId){
+                    //Create selection
+                    var name = validDevId.split(".")[2];
+                    $select.append('<option value="'+devIndex+'">'+name+'</option>');
+
+                    //Create div
+                    console.log("creating "+validDevId);
+                    $div.append('<div class="chromecastDevice_'+devIndex+'"/>');
+                    var $device = $div.find('.chromecastDevice_'+devIndex);
+                    $device.hide();
+
+                    //Connected
+                    $device.append('<input class="chromecastConnected" type="checkbox"/>Connected</br>');
+                    registerForDeviceUpdates($device.find('input.chromecastConnected'), data["oid_connected_"+devIndex]);
+
+                    //Volume
+                    $device.append('<input class="chromecastVolume" type="range" min="0" max="100" />Volume</br>');
+                    registerForDeviceUpdates($device.find('input.chromecastVolume'), data["oid_volume_"+devIndex]);
+
+                    //Muted
+                    $device.append('<input class="chromecastMuted" type="checkbox"/>Muted</br>');
+                    registerForDeviceUpdates($device.find('input.chromecastMuted'), data["oid_muted_"+devIndex]);
+
+                    //Playing
+                    $device.append('<input class="chromecastPlaying" type="checkbox"/>Playing</br>');
+                    registerForDeviceUpdates($device.find('input.chromecastPlaying'), data["oid_playing_"+devIndex]);
+
+                    //Paused
+                    $device.append('<input class="chromecastPaused" type="checkbox"/>Paused</br>');
+                    registerForDeviceUpdates($device.find('input.chromecastPaused'), data["oid_paused_"+devIndex]);
+
+                    //url2play
+                    $device.append('<input class="chromecastUrl2play" type="text"/>url2play</br>');
+                    registerForDeviceUpdates($device.find('input.chromecastUrl2play'), data["oid_url2play_"+devIndex]);
+
+                    //player state
+                    $device.append('<textarea class="chromecastPlayerState" readonly rows=1/>status</br>');
+                    registerForDeviceUpdates($device.find('textarea.chromecastPlayerState'), data["oid_playerState_"+devIndex]);
+                };
+            };
+
+            var currentSelection = data.selDev;
+            $select.change(function(){
                 
-                //url2play - create
-                $div.append('<input class="url2play" type="text"/>URL2play</br>');
-                var $url2play = $div.find('input.url2play');
-                //url2play - register for iobroker state updates
-                var url2play_state = validDevId+".player.url2play";
-                vis.states.bind(url2play_state + '.val', function (e, newVal, oldVal) {
-                    console.log("Updated "+url2play_state);
-                    $url2play.val(newVal);
-                });
-                $url2play.val(vis.states[url2play_state + '.val']);
+                var $device_old = $div.find('div.chromecastDevice_'+currentSelection);
+                var $device_new = $div.find('div.chromecastDevice_'+$select.val());
                 
-                $url2play.change(function (evt) {
-                    console.log("url2play: "+$url2play.val());
-                    vis.setValue(ioBrokerState, $(this).val());
-                });
-            }
+                $device_old.hide();
+                $device_new.show();
+                
+                currentSelection = $select.val();
+            });
+            $select.val(data.selDev).change();            
         }
     },
     
     oid_dev_changed: function(widgetID, view, newId, attr, isCss){
+        
+        var oid2State = {
+            "oid_connected_"   : "status.connected",
+            "oid_volume_"      : "status.volume",
+            "oid_muted_"       : "status.muted",
+            "oid_playing_"     : "status.playing",
+            "oid_paused_"      : "player.paused",
+            "oid_url2play_"    : "player.url2play",
+            "oid_playerState_" : "player.playerState"
+        };
+        
         var mods = [];
         //Get device index
         var index = attr.split("_")[2];
@@ -129,12 +201,11 @@ vis.binds.chromecast = {
             vis.views[view].widgets[widgetID].data[attr] = validDevId;
             mods.push(attr);
             //Set derived props
-            vis.views[view].widgets[widgetID].data["oid_connected_"+index] =
-                validDevId+".status.connected";
-            mods.push("oid_connected_"+index);
-            vis.views[view].widgets[widgetID].data["oid_url2play_"+index] =
-                validDevId+".player.url2play";
-            mods.push("oid_url2play_"+index);
+            for (var oid in oid2State) {
+                vis.views[view].widgets[widgetID].data[oid+index] =
+                    validDevId+"."+oid2State[oid];
+                mods.push(oid+index);
+            }
             
         } else {
             //Reset derived props
